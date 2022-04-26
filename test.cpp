1291d6959a1e4096ce9eeb762b220b2ab9c0774d
Kirill Chilievich <kirillchilievich@Kirills-MacBook-Pro.local>
2022-04-26 11:03:12 +0200

    [WIP] Add procedural rooms generation script (test.cpp)

---
+++ test.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cmath>
+
+int main(int argc, char *argv[]) {
+  srand((unsigned)(time(NULL)));
+
+  int XSize = std::stoi(argv[1]);
+  int YSize = std::stoi(argv[2]);
+  int RoomsAmount = std::stoi(argv[3]);
+
+  float Multiplier = std::sqrt((XSize * YSize) / RoomsAmount);
+  int XRoomSize = XSize / std::round(XSize / Multiplier);
+  int YRoomSize = YSize / std::ceil(YSize / Multiplier);
+
+  // rooms stuff calculated, find out start and end rooms
+
+  bool ChooseVertical = (int)rand() % 2 == 0;
+  int Offset = (rand() % (ChooseVertical ? YSize / YRoomSize : XSize / XRoomSize));
+
+  // choose finish room
+
+  // TODO: Modify to not make absent room finish
+  int FinishOffset = (rand() % (ChooseVertical ? YSize / YRoomSize : XSize / XRoomSize));
+
+  for(int y = 0; y < YSize + 1; y++) {
+    for(int x = 0; x < XSize + 1; x++) {
+      if ((y - 1) / YRoomSize * XSize / XRoomSize + (x - 1) / XRoomSize + 1 > RoomsAmount) {
+        break;
+      }
+
+      if (x / (float)XRoomSize > XSize / XRoomSize) {
+        break;
+      }
+
+      if ((x == 0) || (x >= XRoomSize && (x % XRoomSize == 0))) {
+        std::cout << "X";
+      } else if ((y == 0) || (y >= YRoomSize && (y % YRoomSize == 0))) {
+        std::cout << "X";
+      } else {
+        if (ChooseVertical &&
+            (y / YRoomSize >= Offset) &&
+            (y / YRoomSize <= Offset) &&
+            (x / XRoomSize < 1.f)) {
+          std::cout << "S";
+        } else if (!ChooseVertical &&
+                   (x / XRoomSize >= Offset) &&
+                   (x / XRoomSize <= Offset) &&
+                   (y / YRoomSize < 1.f)) {
+          std::cout << "S";
+        } else if (ChooseVertical &&
+                   (y / YRoomSize >= FinishOffset) &&
+                   (y / YRoomSize <= FinishOffset) &&
+                   (x / XRoomSize == XSize / XRoomSize - 1)) {
+          std::cout << "F";
+        } else if (!ChooseVertical &&
+                   (x / XRoomSize >= FinishOffset) &&
+                   (x / XRoomSize <= FinishOffset) &&
+                   (y / YRoomSize == YSize / YRoomSize - 1)) {
+          std::cout << "F";
+        } else {
+          std::cout << " ";
+        }
+      }
+    }
+    std::cout << "\n";
+  }
+}
